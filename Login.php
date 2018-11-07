763c3b83bf7b1334bf198e5c3dbe660d39eb4f71
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-07 23:53:15 +0200

    update

--- Login.php
+++ Login.php
@@ -7,20 +7,22 @@
     $password = $_POST["password"];
     
     $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ?");
-    mysqli_stmt_bind_param($statement, "s", $username);
+    mysqli_stmt_bind_param($statement, "ss", $username, $password);
     mysqli_stmt_execute($statement);
+
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $colUserID, $colName, $colUsername, $colAge, $colPassword);
+    mysqli_stmt_bind_result($statement, $userID, $name, $username, $age, $password);
     
     $response = array();
     $response["success"] = false;  
     
     while(mysqli_stmt_fetch($statement)){
-        if (password_verify($password, $colPassword)) {
-            $response["success"] = true;  
-            $response["name"] = $colName;
-            $response["age"] = $colAge;
-        }
+        
+        $response["success"] = true;  
+        $response["name"] = $name;
+        $response["age"] = $age;
+        $response["username"] = $username;
+        $response["password"] = $password;
     }
 
     echo json_encode($response);
