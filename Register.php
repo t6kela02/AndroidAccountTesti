d3d60bba4333553a67cf8851e85c60201d211716
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-07 22:49:43 +0200

    update

--- Register.php
+++ Register.php
@@ -1,5 +1,5 @@
 <?php
-    require("password.php");
+    
 
     $connect = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");
     
@@ -8,37 +8,14 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-     function registerUser() {
-        global $connect, $name, $age, $username, $password;
-        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-        $statement = mysqli_prepare($connect, "INSERT INTO user (name, age, username, password) VALUES (?, ?, ?, ?)");
-        mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $passwordHash);
-        mysqli_stmt_execute($statement);
-        mysqli_stmt_close($statement);     
-    }
-
-    function usernameAvailable() {
-        global $connect, $username;
-        $statement = mysqli_prepare($connect, "SELECT * FROM user WHERE username = ?"); 
-        mysqli_stmt_bind_param($statement, "s", $username);
-        mysqli_stmt_execute($statement);
-        mysqli_stmt_store_result($statement);
-        $count = mysqli_stmt_num_rows($statement);
-        mysqli_stmt_close($statement); 
-        if ($count < 1){
-            return true; 
-        }else {
-            return false; 
-        }
-    }
-
+     
+    $statement = mysqli_prepare($connect, "INSERT INTO user (name, age, username, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $password);
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_close($statement);     
+   
     $response = array();
-    $response["success"] = false;  
-
-    if (usernameAvailable()){
-        registerUser();
-        $response["success"] = true;  
-    }
+    $response["success"] = true;  
     
     echo json_encode($response);
 ?>
