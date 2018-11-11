d8fb4b22427a65c804add05bda84eaf3a8e465f0
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-11 23:29:17 +0200

    update

--- Login.php
+++ Login.php
@@ -1,27 +1,28 @@
 <?php
     
 
-    $connect = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");//("my_host", "my_user", "my_password", "my_database");
+    $con = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");//("my_host", "my_user", "my_password", "my_database");
 
     $username = $_POST["username"];
     $password = $_POST["password"];
     
-    $statement = mysqli_prepare($connect, "SELECT * FROM user WHERE username = ?");
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
     mysqli_stmt_bind_param($statement, "ss", $username, $password);
     mysqli_stmt_execute($statement);
-
+    
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $user_id, $name, $username, $age, $password);
-
+    mysqli_stmt_bind_result($statement, $userID, $name, $age, $username, $password);
+    
     $response = array();
     $response["success"] = false;  
     
     while(mysqli_stmt_fetch($statement)){
-        
         $response["success"] = true;  
         $response["name"] = $name;
         $response["age"] = $age;
+        $response["username"] = $username;
+        $response["password"] = $password;
     }
-
+    
     echo json_encode($response);
 ?>
