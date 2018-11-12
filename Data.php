d0abc1386c1f8b06870075ab021390ff9c84769a
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-12 11:32:16 +0200

    Data input

--- Data.php
+++ Data.php
@@ -1,1 +1,19 @@
-
+<?php
+    
+    $connect = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");
+    
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+     
+    $statement = mysqli_prepare($connect, "INSERT INTO data (seconds, beacon_name) VALUES (?, ?)");
+    mysqli_stmt_bind_param($statement, "is", $seconds, $beacon_name);
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_close($statement);     
+   
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
