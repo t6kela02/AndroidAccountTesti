42f95368f129018c0307034ae8b1a8e464a90825
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-12 12:31:31 +0200

    update

---
+++ DataIn.php
@@ -0,0 +1,24 @@
+<?php
+    
+    $connect = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");
+    
+    $user_id = $_POST["user_id"];
+    
+    $statement = mysqli_prepare($connect, "SELECT * FROM data WHERE user_id = ?");
+    mysqli_stmt_bind_param($statement, "i", $user_id);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $data_id, $user_id, $seconds, $beacon_name);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;  
+        $response["seconds"] = $seconds;
+        $response["beacon_name"] = $beacon_name;
+    }
+    
+    echo json_encode($response);
+?>
