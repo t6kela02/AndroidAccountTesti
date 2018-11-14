4d0139a908d3bdd7bdb9933e56e239f67d8ac669
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-14 12:06:47 +0200

    u

--- DataOutTest.php
+++ DataOutTest.php
@@ -3,7 +3,7 @@
     $connect = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");
     
     
-    $sql = "SELECT * FROM data WHERE user_id = 2";
+    $sql = "SELECT * FROM data";
     $result = mysqli_query($connect, $sql);
 
     $json_array = array();
