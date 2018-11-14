cb440e8071834d30b2b0434f8b5c806b806f5fc0
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-14 12:04:24 +0200

    u

--- DataOutTest.php
+++ DataOutTest.php
@@ -2,11 +2,9 @@
     
     $connect = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");
     
-    $user_id = $_POST["user_id"];
     
-    $statement = mysqli_prepare($connect, "SELECT * FROM data WHERE user_id = ?");
-    mysqli_stmt_bind_param($statement, "i", $user_id);
-    mysqli_stmt_execute($statement);
+    
+    $statement = mysqli_prepare($connect, "SELECT * FROM data WHERE user_id = 2");
     
     $json_array = array();
     
