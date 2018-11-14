e2a3a328579715ccbb9fd3c9e031e03c7b100bda
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-14 10:26:55 +0200

    u

--- DataOutTest.php
+++ DataOutTest.php
@@ -8,7 +8,7 @@
     mysqli_stmt_bind_param($statement, "i", $user_id);
     mysqli_stmt_execute($statement);
     
-    mysqli_stmt_store_result($statement);
+    //mysqli_stmt_store_result($statement);
     //mysqli_stmt_bind_result($statement, $data_id, $user_id, $seconds, $beacon_name);
     
     $response = array();
