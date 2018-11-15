769a193c5cfd6d1b958e356d7441ce27884c44d5
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-15 10:29:30 +0200

    u

--- DataOutTest.php
+++ DataOutTest.php
@@ -27,27 +27,40 @@
 
     $con = mysqli_connect("den1.mysql3.gear.host", "accountit", "Ru8tmg~976-i", "accountit");
     $user_id = $_POST["user_id"];
-    
+    /*
     $statement = mysqli_prepare($con, "SELECT * FROM data WHERE user_id = ?");
     mysqli_stmt_bind_param($statement, "i", $user_id);
     mysqli_stmt_execute($statement);
     
     mysqli_stmt_store_result($statement);
     mysqli_stmt_bind_result($statement, $id, $user_id, $seconds, $beacon_name);
-    
+    */
+
+    $selectUser = $con->prepare("SELECT * FROM `data` WHERE `user_id`=?");
+    $selectUser->bind_param('i', $user_id);
+    $selectUser->execute();
+    $result = $selectUser->get_result();
+    //$assoc = $result->fetch_assoc();
+
     $response = array();
-    $response["success"] = false;  
-    
-    while($row = mysqli_stmt_fetch_assoc($statement)){
+    $response["success"] = false;
+
+    while($row = fetch_assoc($result)){
+        $response["success"] = true;
+        //$response[] = $row;
+        $response[] = $row;
+        
+    }
+
+    /*
+    while($row = mysqli_stmt_fetch($statement)){
         $response["success"] = true;
         //$response[] = $row;
         
         $response["seconds"] = $seconds;
         $response["beacon_name"] = $beacon_name;
-       
-        
         
     }
-    
+    */
     echo json_encode($response);
 ?>
