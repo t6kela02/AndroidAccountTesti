bf51174f276a13737f297b146ec5e37904244ff4
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-15 09:39:00 +0200

    u

--- DataOutTest.php
+++ DataOutTest.php
@@ -4,11 +4,13 @@
     
     $user_id = $_POST["user_id"];
 
-    $sql = "SELECT * FROM data WHERE (user_id) VALUES (?)";
-    mysqli_stmt_bind_param($sql, "i", $user_id);
-    mysqli_stmt_execute($sql);
+    $statement = mysqli_prepare($connect, "SELECT * FROM data WHERE (user_id) VALUES (?)");
+    //$sql = "SELECT * FROM data WHERE (user_id) VALUES (?)";
+    mysqli_stmt_bind_param($statement, "i", $user_id);
+    mysqli_stmt_execute($statement);
 
-    $result = mysqli_query($connect, $sql);
+    //$result = mysqli_query($connect, $sql);
+    $result = mysqli_query($statement);
 
     $json_array = array();
 
