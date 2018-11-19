b2e8bf806f4c4d8ccc75a5c97b1c6413ea5bc2da
t6kela02 <32162537+t6kela02@users.noreply.github.com>
2018-11-19 12:33:04 +0200

    Date added

--- DataIn.php
+++ DataIn.php
@@ -5,9 +5,10 @@
     $user_id = $_POST["user_id"];
     $seconds = $_POST["seconds"];
     $beacon_name = $_POST["beacon_name"];
+    $date = $_POST["date"];
      
-    $statement = mysqli_prepare($connect, "INSERT INTO data (user_id, seconds, beacon_name) VALUES (?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "iis", $user_id, $seconds, $beacon_name);
+    $statement = mysqli_prepare($connect, "INSERT INTO data (user_id, seconds, beacon_name, date) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "iiss", $user_id, $seconds, $beacon_name, $date);
     mysqli_stmt_execute($statement);
     mysqli_stmt_close($statement);     
    
